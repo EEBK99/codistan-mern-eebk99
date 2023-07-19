c20d6a9f0f79ba86dcd33dd40c1f6e24626abcb3
Haseeb Khan <haseeb999khan@gmail.com>
2023-07-19 18:04:57 +0500

    optimizing and responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import logo3 from "../assets/images/logo3.png";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-fiord-500  w-full">
+    <footer className="bg-fiord-500 w-full">
       <div className="flex justify-between items-center py-1 px-10 container mx-auto">
-        <div>
+        <div className="grayscale">
           <img src={logo3} alt="logo1" />
         </div>
-        <div className="text-white flex items-center">
+        <div className="text-fiord-400 flex items-center">
           <p>COPYRIGHT 2016. ALL RIGHTS RESERVED</p>
         </div>
       </div>
